d206b0efca4669eb7d57ff58b7d33af9d4db16cc
Benedict Thompson <benjfthompson@gmail.com>
2021-01-29 19:07:00 +0000

    Starting work on map generation

---
+++ Assets/Scripts/NoiseGenerator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoiseGenerator : MonoBehaviour
+{
+    public static float[,] Generate(int width, int height, float scale, Wave[] waves, Vector2 offset) {
+        return null;
+    }
+}
+
+[System.Serializable]
+public class Wave {
+    public float seed,
+        frequency,
+        amplitude;
+}
