5b9ca18ae8abb75b3873f7773941b140634d4d85
Mike <mikeyallanmda@gmail.com>
2021-01-29 23:58:57 +0000

    Patrol added

--- Assets/Scripts/AiPath.cs
+++ Assets/Scripts/AiPath.cs
@@ -2,17 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AiPath : MonoBehaviour
+public class AiPath
 {
-    // Start is called before the first frame update
-    void Start()
+    public Vector2 Position { get; private set; }
+    public string PathState { get; set; }
+    public Vector2[] Pathing { get; private set; }
+    private int pathi = 0;
+    public AiPath(Vector2 position, string pathState, Vector2[] pathing)
     {
-        
+        Position = position;
+        PathState = pathState;
+        Pathing = pathing;
     }
-
-    // Update is called once per frame
-    void Update()
+    public void Move(float speed)
     {
-        
+        if (PathState == "patrol") Patrol(speed);
+    }
+    private void Patrol(float speed)
+    {
+        if (Position.x < Pathing[pathi].x) Position = new Vector2(Position.x + speed, Position.y);
+        else if(Position.x > Pathing[pathi].x) Position = new Vector2(Position.x - speed, Position.y);
+        if (Position.y < Pathing[pathi].y) Position = new Vector2(Position.x, Position.y + speed);
+        else if(Position.y > Pathing[pathi].y) Position = new Vector2(Position.x, Position.y - speed);
+        if (Position.x == Pathing[pathi].x && Position.y == Pathing[pathi].y)
+        {
+            pathi++;
+            if (pathi == Pathing.Length) pathi = 0;
+        }
     }
 }
