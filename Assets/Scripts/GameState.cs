e7b088325ed615b130e41525d62d82f39befb469
Mike <mikeyallanmda@gmail.com>
2021-01-30 11:19:59 +0000

    Node added

--- Assets/Scripts/GameState.cs
+++ Assets/Scripts/GameState.cs
@@ -10,14 +10,6 @@ public class GameState : MonoBehaviour
         var ai = new AiPath(new Vector2(0f,0f), "patrol", new Vector2[] { new Vector2(5f,5f), new Vector2(0f, 0f) });
         ai.Move(2f);
         Debug.Log($"pos: {ai.Position} x: {ai.Position.x} y: {ai.Position.y}");
-        ai.Move(2f);
-        Debug.Log($"pos: {ai.Position} x: {ai.Position.x} y: {ai.Position.y}");
-        ai.Move(2f);
-        Debug.Log($"pos: {ai.Position} x: {ai.Position.x} y: {ai.Position.y}");
-        ai.Move(2f);
-        Debug.Log($"pos: {ai.Position} x: {ai.Position.x} y: {ai.Position.y}");
-        ai.Move(2f);
-        Debug.Log($"pos: {ai.Position} x: {ai.Position.x} y: {ai.Position.y}");
     }
     void Update()
     {
---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    public Node Parent { get; set; }
+    public GameObject Object { get; set; }
+
+    //The distance is essentially the estimated distance, ignoring walls to our target. 
+    //So how many tiles left and right, up and down, ignoring walls, to get there. 
+    //public void SetDistance(int targetX, int targetY)
+    //{
+    //    this.Distance = Math.Abs(targetX - Object.transform.position.x) + Math.Abs(targetY - Object.transform.position.y);
+    //}
+}
