66865fbe9a399c88390274c73293d425e28aab39
Nassim Rhaiem <rhaiemn@coventry.ac.uk>
2021-01-30 00:01:26 +0000

    Added assets and random movement directions

--- Assets/Scripts/AiController.cs
+++ Assets/Scripts/AiController.cs
@@ -6,27 +6,61 @@ public class AiController : MonoBehaviour
 {
     public float speed = 3.0f;
     public Vector2 Position = new Vector2(0,0);
+    
     public List<Vector2> Patrol = new List<Vector2>();
-    Rigidbody2D rigidbody2d;
-    private float xDirection;
-    private float yDirection;
+    public float changeTime = 3.0f;
+    
+    Rigidbody2D rigidbody2d; // new added to remove warning
+
+    float timer;
+    int direction = 1;
+    
+
+    private float Direction;
+    private bool vertical;
 
     void Start()
     {
+        timer = changeTime;
         rigidbody2d = GetComponent<Rigidbody2D>();
-        xDirection = (float)Random.Range(0f, 0.3f);
-        yDirection = (float)Random.Range(0f, 0.3f);
+        Direction = (float)Random.Range(0f, 1f); // random direction up or down random range is range
+        if (Direction >= 0.5f)
+            vertical = true;
+
+        else
+            vertical = false;
+
+        
+        Debug.Log(Direction);
     }
 
     void Update()
     {
-        
+        timer -= Time.deltaTime;
+
+        if (timer < 0)
+        {
+            direction = -direction;
+            timer = changeTime;
+        }
     }
     void FixedUpdate()
     {
-        var position = rigidbody2d.position;
-        position.x += speed * xDirection * Time.deltaTime;
-        position.y += speed * yDirection * Time.deltaTime;
+        Vector2 position = rigidbody2d.position;
+
+        if (vertical)
+        {
+            position.y += Time.deltaTime * speed * direction;
+            //animator.SetFloat("Move X", 0);
+            //animator.SetFloat("Move Y", direction);
+        }
+        else
+        {
+            position.x += Time.deltaTime * speed * direction;
+            //animator.SetFloat("Move X", direction);
+            //animator.SetFloat("Move Y", 0);
+        }
+
         rigidbody2d.MovePosition(position);
     }
 }
--- Assets/Scripts/GenerateEnemies.cs
+++ Assets/Scripts/GenerateEnemies.cs
@@ -9,12 +9,10 @@ public class GenerateEnemies : MonoBehaviour
     // search
     public static void Generate(Vector2Int StartPosition, int id)
     {
-        var enemy = Resources.Load("Prefabs/Enemy") as GameObject;
+        var enemy = Resources.Load("Prefabs/darknessF") as GameObject;
         enemy.name = $"enemy {id}";
         AiController obj = enemy.GetComponent<AiController>();
-        Debug.Log("here1");
         //obj.Position = new Vector2(1, 3);
-        Debug.Log("here2");
         Instantiate(obj);
     }
 }
