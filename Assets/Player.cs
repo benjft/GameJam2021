1c3681b3646d5750512dfde328670d38fa981526
Adam Lane <adamsadlerlane@gmail.com>
2021-01-30 22:35:51 +0000

    health bar complete with example of how to attach to object

---
+++ Assets/Player.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int max_willpower = 100;
+    public int current_willpower;
+
+    public Willpower willbar;
+    public Willpower willbar2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        current_willpower = max_willpower;
+        willbar.SetMaxWillpower(max_willpower);
+        willbar2.SetMaxWillpower(max_willpower);
+        willbar2.SetWillpower(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TakeDamage(10);
+        }
+
+
+    }
+    void TakeDamage(int damage)
+    {
+        current_willpower -= damage;
+
+        willbar.SetWillpower(current_willpower);
+        willbar2.SetWillpower(max_willpower - current_willpower);
+    }
+}
